e38118268e1a8d96caa4a8cf7147096a4d5feec7
cryptokeepto <cryptokeepto@gmail.com>
2020-08-02 23:05:47 +0700

    Add config firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDae4t0-qjRuQ1ijbeG97i8xjfHRj8cYZQ",
+  authDomain: "tik-tok-clone-6f0cd.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-6f0cd.firebaseio.com",
+  projectId: "tik-tok-clone-6f0cd",
+  storageBucket: "tik-tok-clone-6f0cd.appspot.com",
+  messagingSenderId: "497626296797",
+  appId: "1:497626296797:web:ea97c0fbb056b9be3b4634",
+  measurementId: "G-M39FJRZE1M",
+};
