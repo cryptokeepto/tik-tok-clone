ae07ad4cfa426b9940f86b6d5eac50aa43fbd5de
cryptokeepto <cryptokeepto@gmail.com>
2020-08-09 21:36:45 +0700

    Footer add icon spinner and text

--- src/VideoFooter.tsx
+++ src/VideoFooter.tsx
@@ -5,7 +5,15 @@ import "./VideoFooter.css";
 function VideoFooter() {
   return (
     <div className="videoFooter">
-      <h2>I am a footer</h2>
+      <div className="videoFooter__text">
+        <h3>@cryptokeepto</h3>
+        <p>this is a description</p>
+      </div>
+      <img
+        src="https://static.thenounproject.com/png/934821-200.png"
+        alt=""
+        className="videoFooter__record"
+      />
     </div>
   );
 }
