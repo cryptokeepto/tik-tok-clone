ca9df5b8ea1f5abcec45104c70640eab92f64869
cryptokeepto <cryptokeepto@gmail.com>
2020-08-09 21:06:09 +0700

    Show video and snap

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import "./Video.css";
+
+import VideoFooter from "./VideoFooter";
 
 const Video = (): JSX.Element => {
-  return <div>video</div>;
+  const [playing, setPlaying]: any = useState(false);
+  const videoRef: any = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={onVideoPress}
+        loop
+        src="https://v16m.tiktokcdn.com/bc662343d4754b30b10f2f2b17556a49/5f329d8a/video/tos/alisg/tos-alisg-pve-0037/80c8fa00c9604107a2601778babf0e35/?a=1180&br=2864&bt=1432&cr=0&cs=0&dr=3&ds=3&er=&l=202008091330490101151531321F0C8BC5&lr=tiktok&mime_type=video_mp4&qs=0&rc=anZuamc5aXY5dTMzaTgzM0ApZmg2NzQ4ODtpNzpmNjs0OmdncjNlZl80cnJfLS1eLzRzczYyLTVhXi82LmI1YGEvLzE6Yw%3D%3D&vl=&vr="
+      ></video>
+      <VideoFooter />
+    </div>
+  );
 };
 
 export default Video;
---
+++ src/VideoFooter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoFooter() {
+  return (
+    <div>
+      <h2>I am a footer</h2>
+    </div>
+  );
+}
+
+export default VideoFooter;
